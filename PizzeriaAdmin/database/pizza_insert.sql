35029b1e50883cce92c6b071aa8ea20918c692e2
redohke <joukkue.red@gmail.com>
2014-09-22 13:37:25 +0300

    moi

---
+++ PizzeriaAdmin/database/pizza_insert.sql
@@ -0,0 +1,42 @@
+insert into pizza (id, nimi, hinta) values (1, 'Margherita', 7.5);
+
+insert into pizza (id, nimi, hinta) values (2, 'Salami', 7.5);
+
+
+insert into pizza(id, nimi, hinta) values (3, 'Francescana', 7.5);
+
+insert into pizza(id, nimi, hinta) values (4, 'Americano', 8.5);
+
+insert into pizza(id, nimi, hinta) values (5, 'Frutti di Mare', 8.5);
+
+
+insert into pizza(id, nimi, hinta) values (6, 'Opera', 8.5);
+
+
+insert into pizza(id, nimi, hinta) values (7, 'Dillinger', 9.5);
+
+
+insert into pizza(id, nimi, hinta) values (8, 'Unelma', 9.5);
+
+
+insert into pizza(id, nimi, hinta) values (9, 'MehukasMegaMättö', 9.5);
+
+
+
+insert into pizza(id, nimi, hinta) values (10, 'Empire', 9.5);
+
+
+insert into pizza(id, nimi, hinta) values (11, 'Pollo', 9.5);
+
+
+insert into pizza(id, nimi, hinta) values (12, 'Charmander', 9.5);
+
+
+insert into pizza(id, nimi, hinta) values (13, 'Vege', 9.5);
+
+
+insert into pizza(id, nimi, hinta) values (14, 'Tyyni Special', 9.5);
+
+
+
+insert into pizza(id, nimi, hinta) values (15, 'Mexicano', 9.5);
