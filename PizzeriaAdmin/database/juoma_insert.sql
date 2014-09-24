c30f72f7468c760dc9e12a53cfed46429b499bd2
a1300929 <a1300929@pao5008w16.haagahelia.amk>
2014-09-24 14:20:57 +0300

    juomat

---
+++ PizzeriaAdmin/database/juoma_insert.sql
@@ -0,0 +1,13 @@
+insert into juoma (id, nimi, hinta) values (1, 'Coca-Cola 0,5 l', 1.70);
+
+insert into juoma (id, nimi, hinta) values (2, 'Coca-Cola 1,5 l', 2.80);
+
+
+insert into juoma (id, nimi, hinta) values (3, 'Fanta 0,5 l', 1.70);
+
+insert into juoma (id, nimi, hinta) values (4, 'Fanta 1,5 l', 2.80);
+
+
+insert into juoma (id, nimi, hinta) values (5, 'Sprite 0,5 l', 1.70);
+
+insert into juoma (id, nimi, hinta) values (6, 'Sprite 1,5 l', 2.80);
